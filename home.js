9b33703298a7e2ea8396fd6d13c183fe61f11ecb
scratchcamp2023 <scratchcamp2023@gmail.com>
2026-09-12 07:20:47 +0900

    Link featured clues to detail pages

--- home.js
+++ home.js
@@ -3,6 +3,6 @@
  const d=await loadData();
  document.querySelector('#featuredCountries').innerHTML=d.countries.slice(0,4).map(countryCard).join('');
  const featured=d.clues.filter(x=>x.status==='verified').slice(0,4);
- document.querySelector('#featuredClues').innerHTML=featured.map(c=>`<a class="mini-clue" href="country.html?id=${c.country}">
+ document.querySelector('#featuredClues').innerHTML=featured.map(c=>`<a class="mini-clue" href="clue.html?id=${c.id}">
  <div class="mini-clue-top">${statusPill(c.status)}<span class="kicker">${c.confidence}</span></div><h3>${c.name}</h3><p>${c.summary}</p></a>`).join('');
 })();
